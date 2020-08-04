f2b3f4ccb77c228c7d976f5519691c178a8a8e4a
Luis G <lglaser@cs.uni-frankfurt.de>
2020-08-04 11:40:55 +0200

    Remove useless test

--- test_main.py
+++ test_main.py
@@ -3,9 +3,6 @@ from debuggo.solve import solver
 from debuggo.types import SolvingHistory
 from debuggo.transform import transform
 
-def test_test():
-    print("Test test")
-    assert True
 
 def test_full():
     paths = main()
@@ -33,7 +30,14 @@ def test_abstract_transformer():
         error = e
     assert isinstance(error, NotImplementedError)
 
-
+def test_holds_transformer():
+    ht = transform.HoldsTransformer()
+    with open("tests/program.lp", encoding="utf-8") as f:
+        program = ht.transform("".join(f.readlines()))
+    
+    with open("tests/program_transformed_holds.lp", encoding="utf-8") as f:
+        gold_result = "".join(f.readlines())
+    assert program == gold_result
 
 ## Solver History and State
 def testSolverHistoryType():
