44f1786cdd25fa10a2ee6aa1a703d55a7e3e9a51
Luis Glaser <Luis.Glaser@stud.uni-frankfurt.de>
2020-10-26 16:20:43 +0100

    Remove old solver

--- debuggo/solve/solver.py
+++ debuggo/solve/solver.py
@@ -3,51 +3,10 @@ import clingo
 import networkx as nx
 
 
-class Solver():
-    
-    def __init__(self, stable_models):
-        self.stable_models = stable_models
-
-    def get_branch(self, head, stable_models):
-        trues, falses = [], []
-        for stable_model in stable_models:
-            if head in stable_model:
-                trues.append(stable_model)
-            else:
-                falses.append(stable_model)
-        return trues, falses
-
-    def next_try(self, prev_stack_until_now, definition_history, i):
-        trues, falses = self.get_branch(definition_history[i], self.stable_models)
-        # abbruch bedingung
-        if i == len(definition_history) - 1:
-            final_paths = []
-            if len(trues):
-                new_path = prev_stack_until_now + [definition_history[i]]
-                final_paths.append(new_path)
-            if len(falses):
-                new_path = prev_stack_until_now + [f"not_{definition_history[i]}"]
-                final_paths.append(new_path)
-            return final_paths
-        else:
-            paths = []
-            if len(trues):
-                new_path = prev_stack_until_now + [definition_history[i]]
-                paths.extend(self.next_try(new_path, definition_history, i+1))
-            if len(falses):
-                new_path = prev_stack_until_now + [f"not_{definition_history[i]}"]
-                paths.extend(self.next_try(new_path, definition_history, i+1))
-            return paths
-
-    def generate_path(self, definition_history):
-        paths = self.next_try([], definition_history, 0)
-        return paths
-
-
 
 class SolveRunner():
     """
-    Interacts with the clingo solver to produce a full solving history. 
+    Interacts with the clingo solver to produce a full solving history.
     """
     def __init__(self, program="", control=clingo.Control()):
         self.program = program
@@ -62,7 +21,7 @@ class SolveRunner():
         self.prev = None
         self.current_model = None
         self._step_count = 0
-    
+
     def ground(self):
         self.ctl.ground([("base", [])])
 
@@ -81,17 +40,19 @@ class SolveRunner():
         self.prev = model
 
     def solver_has_reached_stable_model(self):
+        print(self.current_model)
+        print(self.prev)
         if self.current_model == self.prev:
             print("Solver has reached a stable model.")
             return True
         else:
             print("Solving..")
-            return False 
-    
+            return False
+
     def step_until_stable(self):
-        while not self.solver_has_reached_stable_model():
+        while self._step_count < 1 or not self.solver_has_reached_stable_model():
             self.step()
-    
+
     def step(self):
         #ctl.solve(on_model=on_model)
         true_externals = []
@@ -110,23 +71,23 @@ class SolveRunner():
                 #ctl.assign_external(clingo.String("d"),True)
         self.current_model = SolverState(self.remove_holds_atoms_from_model(symbols_in_model), self._step_count)
         if (not self.solver_has_reached_stable_model()):
-            
+
             self.add_model_to_history(self.current_model, self.program.split("\n")[rule_no_because_of_which_true_externals_are_true])
 
             self.update_externals(true_externals)
         self._step_count += 1
-    
+
     def remove_holds_atoms_from_model(self, model):
         cleaned_model = set()
         for symbol in model:
             if not symbol.name == "h":
                 cleaned_model.add(symbol)
         return cleaned_model
-    
+
 
 class SolverState():
-    """ 
-    Represents a single solver state that is created during execution. 
+    """
+    Represents a single solver state that is created during execution.
     A SolverState consists of the current stable model, what became true and what became false
     after the last step.
 
@@ -136,10 +97,10 @@ class SolverState():
     def __init__(self, model, step = 0):
         self.model = model
         self.step = step
-    
+
     def __repr__(self):
         return f"{self.model}"
-    
+
     def __eq__(self, other):
         if isinstance(other, SolverState):
             return self.model == other.model
