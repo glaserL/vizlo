de298e08352bf27d0333416a32b081c5ae54db67
Luis Glaser <Luis.Glaser@stud.uni-frankfurt.de>
2020-10-29 12:20:36 +0100

    Useless test

--- tests/test_solving.py
+++ tests/test_solving.py
@@ -3,21 +3,26 @@ from debuggo.types import SolvingHistory
 
 
 def get_transformed_test_program():
-    with open("tests/program_transformed_holds.lp", encoding="utf-8") as f:
+    with open("/Users/glaser/Developer/cogsys/3_cci/debuggo/tests/program_transformed_holds.lp", encoding="utf-8") as f:
         reified_program = "".join(f.readlines())
     return reified_program
 
-def test_solver_prototype():
-    stable_models = [["a"]]
-    s = solver.Solver(stable_models)
-    assert stable_models == s.stable_models
 
 def test_single_solver_runner_step():
     reified_program = get_transformed_test_program()
     sr = solver.SolveRunner(reified_program)
+    sr.ctl.ground([("base", [])])
     for _ in range(5):
         print(".",end="")
         sr.step()
+    graph = sr.graph
+    assert len(graph) == 5
+
+def test_until_stable():
+    reified_program = get_transformed_test_program()
+    sr = solver.SolveRunner(reified_program)
+    sr.ctl.ground([("base", [])])
+    sr.step_until_stable()
     graph = sr.graph
     assert len(graph) == 5
 
