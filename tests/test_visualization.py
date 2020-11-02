1676c23aceaf869f9dcd1b73905bb131ec0c262a
Luis Glaser <Luis.Glaser@stud.uni-frankfurt.de>
2020-11-02 13:58:19 +0100

    Add mpl test

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,3 +1,15 @@
-
+import networkx as nx
+import matplotlib.pyplot as plt
 from debuggo.display import folder_display
+from debuggo.solve.solver import SolverState
+from debuggo.display.folder_display import HeadlessPysideDisplay
+def test_print_picture():
+    graph = nx.DiGraph()
+    a = SolverState("a", 0)
+    b = SolverState("b", 1)
+    graph.add_edge(a, b, rule="rule")
+    display = HeadlessPysideDisplay(graph)
+    pic = display.get_graph_as_np_array()
+    plt.imshow(pic)
+    plt.show()
 
