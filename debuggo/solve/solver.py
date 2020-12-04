6a4993348a97e50bb5fdde09dbd07fd21cb568a7
Luis Glaser <Luis.Glaser@stud.uni-frankfurt.de>
2020-12-04 14:50:53 +0100

    refactored solver runner

--- debuggo/solve/solver.py
+++ debuggo/solve/solver.py
@@ -1,3 +1,4 @@
+from typing import List
 
 import clingo
 import networkx as nx
@@ -64,6 +65,16 @@ class SolveRunner():
             s.falses = falses
 
 
+    def _generate_with_recursive(self, base_program, initial_partial_model: SolverState, recursive_component):
+        changed = True
+        previous_partial_model = initial_partial_model
+        while changed:
+            for rule in recursive_component:
+                # We call the recursive component with one of the recursive rules
+                ctl = self._make_new_control_and_ground(base_program+rule)
+                assumptions = self.create_true_symbols_from_solver_state(previous_partial_model)
+
+
     def make_graph(self):
 
         # TODO: REFACTOR THIS FUCKING ABOMINATION
@@ -73,34 +84,48 @@ class SolveRunner():
         for i, rule in enumerate(self.prg):
             print(f"========{i}========Adding rule {rule}")
             current_prg.append(rule)
-            ctl = clingo.Control("0")
-            ctl.add("base", [], "\n".join(current_prg))
-            ctl.ground([("base", [])])
+            ctl = self._make_new_control_and_ground(current_prg)
             # analytically find recursive components and add them at once
-            assumptions = self.find_nodes_at_timestep(i)
-            print(f"{rule} with {len(assumptions)} assumpts.")
-            for assmpts in assumptions:
-                #print(f"Continuing with {assmpts}")
-                assumpts = self.create_true_symbols_from_solver_state(assmpts)
-                print(f"Assumptions: {assumpts}")
-                with ctl.solve(assumptions=assumpts, yield_=True) as handle:
-                    solver_states_to_create = []
-                    hacky_counter = 0
-                    for m in handle:
-                        syms = SolverState(m.symbols(atoms=True), i+1)
-                        print(f"({assmpts})-[{rule}]->({(i + 1, syms)})")
-                        solver_states_to_create.append(syms)
-                        hacky_counter += 1
-                    if hacky_counter == 0:
-                        # HACK: This means the candidate model became conflicting.
-                        solver_states_to_create.append(SolverState(set()))
-                    self.update_falses_in_solver_states(solver_states_to_create)
-                    self.assert_falses_from_assumptions(solver_states_to_create, assumpts)
-                    for ss in solver_states_to_create:
-                        self._g.add_edge(assmpts, ss, rule=rule)
+            partial_models = self.find_nodes_at_timestep(i)
+            print(f"{rule} with {len(partial_models)} previous partial models.")
+            for partial_model in partial_models:
+                #print(f"Continuing with {partial_model}")
+                assumptions = self.create_true_symbols_from_solver_state(partial_model)
+                print(f"Assumptions: {assumptions}")
+                new_partial_models = self._get_new_partial_models(assumptions, ctl, i)
+                self._consolidate_new_solver_states(assumptions, new_partial_models)
+                self._update_graph(partial_model, rule, new_partial_models)
         return self._g
 
-    def assert_falses_from_assumptions(self, syms : SolverState, assumpts):
+    def _get_new_partial_models(self, assumptions, ctl, i):
+        solver_states_to_create = []
+        with ctl.solve(assumptions=assumptions, yield_=True) as handle:
+            hacky_counter = 0
+            for m in handle:
+                syms = SolverState(m.symbols(atoms=True), i + 1)
+                # print(f"({partial_model})-[{rule}]->({(i + 1, syms)})")
+                solver_states_to_create.append(syms)
+                hacky_counter += 1
+            if hacky_counter == 0:
+                # HACK: This means the candidate model became conflicting.
+                solver_states_to_create.append(SolverState(set()))
+        return solver_states_to_create
+
+    def _update_graph(self, assmpts, rule, solver_states_to_create):
+        for ss in solver_states_to_create:
+            self._g.add_edge(assmpts, ss, rule=rule)
+
+    def _consolidate_new_solver_states(self, assumpts, solver_states_to_create):
+        self.update_falses_in_solver_states(solver_states_to_create)
+        self.assert_falses_from_assumptions(solver_states_to_create, assumpts)
+
+    def _make_new_control_and_ground(self, current_prg):
+        ctl = clingo.Control("0")
+        ctl.add("base", [], "\n".join(current_prg))
+        ctl.ground([("base", [])])
+        return ctl
+
+    def assert_falses_from_assumptions(self, syms: List[SolverState], assumpts):
         for sym in syms:
             for atom, value in assumpts:
                 print(f"Updating {sym} with {atom}={value}")
