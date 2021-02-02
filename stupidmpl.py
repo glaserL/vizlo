7d17d65ab7f07fbbf1aef3ba84024183b4b029e1
Luis G <lglaser@cs.uni-frankfurt.de>
2021-02-02 21:00:07 +0100

    cleanup

--- stupidmpl.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-img = mpimg.imread('/Users/glaser/Downloads/Italian-Food-Spaghetti-Bolognese.jpg')
-imgplot = plt.imshow(img)
-plt.show()
